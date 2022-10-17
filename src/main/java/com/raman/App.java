86252d5862dcb32809f8ca1d20d57bbc4474c9b6
cloudraju407 <91491103+cloudraju407@users.noreply.github.com>
2022-10-17 09:54:53 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Testing in progress- Build Pipeline- by Automation Process done-Raju Tester team1" );
+        System.out.println( "Testing in progress- Build Pipeline- by Automation Process done-Raju Tester team2" );
     }
 }
